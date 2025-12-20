1f8d3d3e445f1a2e32233e154e88fe9d4a33f41b
hoanglmv <fire.kidboy1505@gmail.com>
2025-12-20 21:40:36 +0700

    update single gpu scripts

---
+++ scripts/single_gpu/TimeLLM_M4.sh
@@ -0,0 +1,162 @@
+export CUDA_VISIBLE_DEVICES=0
+model_name=TimeLLM
+
+train_epochs=50
+llama_layers=32
+batch_size=24
+learning_rate=0.001
+d_model=8
+d_ff=32
+
+comment='TimeLLM-M4'
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Monthly' \
+  --model_id m4_Monthly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Yearly' \
+  --model_id m4_Yearly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Weekly' \
+  --model_id m4_Weekly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Daily' \
+  --model_id m4_Daily \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Quarterly' \
+  --model_id m4_Quarterly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
+
+
+python run/run_m4.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Hourly' \
+  --model_id m4_Hourly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --llm_layers $llama_layers \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --patch_len 1 \
+  --stride 1 \
+  --batch_size $batch_size \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate $learning_rate \
+  --loss 'SMAPE' \
+  --train_epochs $train_epochs \
+  --model_comment $comment
---
+++ scripts/single_gpu/TimeLLM_Weather_GPT2.sh
@@ -0,0 +1,37 @@
+export CUDA_VISIBLE_DEVICES=0
+model_name=TimeLLM
+train_epochs=10
+learning_rate=0.01
+gpt_layers=12
+
+batch_size=4
+
+comment='TimeLLM-Weather-GPT2-Base'
+
+python run/run_main.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_512_96 \
+  --model $model_name \
+  --data Weather \
+  --features M \
+  --seq_len 512 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --d_model 32 \
+  --d_ff 32 \
+  --batch_size $batch_size \
+  --learning_rate $learning_rate \
+  --llm_model 'GPT2' \
+  --llm_dim 768 \
+  --llm_layers $gpt_layers \
+  --train_epochs $train_epochs \
+  --model_comment $comment
